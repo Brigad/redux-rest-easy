e6ee360add3aa025009f4cbf6fee90c6f37fd08a
Adrien HARNAY <adrien@harnay.me>
2018-02-12 09:24:22 +0100

    refactor(initializeNetwork): clarify name (#11)
    Change initializeNetwork to
    initializeNetworkHelpers
    
    BREAKING CHANGE: change public
    function name (initializeNetwork is
    now initializeNetworkHelpers)

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ export { default as connect } from './connect';
 export { default as createResource } from './createResource';
 export { default as reducer } from './reducer';
 export { default as reset } from './reset';
-export { default as initializeNetwork } from './initializeNetwork';
+export {
+  default as initializeNetworkHelpers,
+} from './initializeNetworkHelpers';
