abafcad7e5f0f0c443a3b74078c06850281fffa6
Adrien HARNAY <adrien@harnay.me>
2018-03-06 18:07:33 +0100

    feat(console errors): make errors actionable (#22)
    Provide link to documentation when displaying errors

--- src/internals/utils/getState.js
+++ src/internals/utils/getState.js
@@ -2,9 +2,11 @@
 
 const getState = (state) => {
   if (!state || !state.restEasy) {
-    console.error(
-      'There doesn\'t seem to be a "restEasy" key in your state. In doubt, double check the docs on reducers.',
-    );
+    console.error(`
+      There doesn't seem to be a "restEasy" key in your state.
+
+      For more information, browse the related documentation: https://github.com/Brigad/redux-rest-easy/blob/master/docs/api/reducer.md#reducer
+    `);
     return null;
   }
 
