69a56063c69ebe76de99e7cd65454730d5f52db0
Adrien HARNAY <adrien@harnay.me>
2018-04-30 19:37:50 +0200

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,53 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import builtins from 'rollup-plugin-node-builtins';
 import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 
 const env = process.env.NODE_ENV;
-
 const config = {
   input: 'src/index.js',
+  plugins: [
+    nodeResolve(),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+  ],
   external: ['react', 'react-redux'],
-  output: {
-    name: 'ReduxRestEasy',
+  context: 'window',
+};
+
+if (env === 'es' || env === 'cjs') {
+  config.output = {
+    format: env,
+  };
+  config.plugins.push(
+    babel({
+      plugins: ['external-helpers'],
+    }),
+  );
+}
+
+if (env === 'development' || env === 'production') {
+  config.output = {
     format: 'umd',
+    name: 'ReduxRestEasy',
+    indent: false,
     globals: {
       react: 'React',
       'react-redux': 'ReactRedux',
     },
-  },
-  context: 'window',
-  plugins: [
-    nodeResolve({
-      preferBuiltins: true,
-    }),
-    builtins(),
+  };
+  config.plugins.push(
     babel({
-      exclude: '**/node_modules/**',
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env),
     }),
-    commonjs(),
-  ],
-};
+  );
+}
 
 if (env === 'production') {
   config.plugins.push(
