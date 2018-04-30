d7ab011d1a432aa38d6c01cfc1ff319403af6a42
Adrien HARNAY <adrien@harnay.me>
2018-04-30 20:36:25 +0200

    done

--- rollup.config.js
+++ rollup.config.js
@@ -1,65 +1,63 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
+import pkg from './package.json';
 
 const env = process.env.NODE_ENV;
+const isUMD = env !== 'es' && env !== 'cjs';
+
 const config = {
   input: 'src/index.js',
+  output: !isUMD
+    ? {
+        format: env,
+      }
+    : {
+        format: 'umd',
+        name: 'ReduxRestEasy',
+        indent: false,
+        globals: {
+          react: 'React',
+          'react-redux': 'ReactRedux',
+        },
+      },
+  external: !isUMD
+    ? [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+      ]
+    : Object.keys(pkg.peerDependencies || {}),
+  context: 'window',
   plugins: [
-    nodeResolve(),
+    resolve({
+      jsnext: true,
+      main: true,
+    }),
     commonjs({
       include: 'node_modules/**',
     }),
-  ],
-  external: ['react', 'react-redux'],
-  context: 'window',
-};
-
-if (env === 'es' || env === 'cjs') {
-  config.output = {
-    format: env,
-  };
-  config.plugins.push(
-    babel({
-      plugins: ['external-helpers'],
-    }),
-  );
-}
-
-if (env === 'development' || env === 'production') {
-  config.output = {
-    format: 'umd',
-    name: 'ReduxRestEasy',
-    indent: false,
-    globals: {
-      react: 'React',
-      'react-redux': 'ReactRedux',
-    },
-  };
-  config.plugins.push(
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers'],
     }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(env),
-    }),
-  );
-}
-
-if (env === 'production') {
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false,
-      },
-    }),
-  );
-}
+    isUMD
+      ? replace({
+          'process.env.NODE_ENV': JSON.stringify(env),
+        })
+      : null,
+    env === 'production'
+      ? uglify({
+          compress: {
+            pure_getters: true,
+            unsafe: true,
+            unsafe_comps: true,
+            warnings: false,
+          },
+        })
+      : null,
+  ].filter(Boolean),
+};
 
 export default config;
